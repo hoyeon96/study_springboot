eeed4b73f3bd58667ef77a11c9a51f4e046a166d
hoyeon96 <84115607+hoyeon96@users.noreply.github.com>
2022-03-30 18:17:24 +0900

    3-3 HTML template

--- controller-demo/src/main/java/dev/yeon/controllerdemo/SampleController.java
+++ controller-demo/src/main/java/dev/yeon/controllerdemo/SampleController.java
@@ -3,37 +3,43 @@ package dev.yeon.controllerdemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("view")
 public class SampleController {
     private static final Logger logger
             = LoggerFactory.getLogger(SampleController.class);
 
+    @GetMapping("/sample-jsp")
+    public String sampleJsp(Model model){
+        logger.info("in sample jsp");
+        List<SamplePayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePayload("Adam", 22, "Student"));
+        profiles.add(new SamplePayload("김태훈", 21, "Developer"));
+        profiles.add(new SamplePayload("이승현", 24, "Designer"));
 
-    @RequestMapping(
-            value = "hello",
-            method = RequestMethod.GET
-    )
-    public String hello(@RequestParam(name = "id", required = false, defaultValue = "") String id){
-        logger.info("Path: Hello");
-        logger.info("Query Param id: " + id);
-        return "/hello.html";
+        model.addAttribute("profiles", profiles);
+
+        return "view-jsp";
     }
 
-    @GetMapping(
-            value = "/hello/{id}"
-    )
-    public String helloPath(@PathVariable("id") String id){
-        logger.info(id);
-        return "/hello.html";
-    }
+    @GetMapping("/sample-thyme")
+    public ModelAndView sampleThyme(){
+        logger.info("in sample thyme");
+        ModelAndView modelAndView = new ModelAndView();
+        List<SamplePayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePayload("Adam", 22, "Student"));
+        profiles.add(new SamplePayload("김태훈", 21, "Developer"));
+        profiles.add(new SamplePayload("이승현", 24, "Designer"));
 
-    @GetMapping(
-            "/get-profile"
-    )
-    public @ResponseBody SamplePayload getProfile(){
-        return new SamplePayload("devyeon", 27, "Developer");
+        modelAndView.addObject("profiles", profiles);
+        modelAndView.setViewName("view-thyme");
+        return modelAndView;
     }
-
 }
