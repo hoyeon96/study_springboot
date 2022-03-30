b7d33cc7afdf8381ef2e52a1b43a9128560f3984
hoyeon96 <84115607+hoyeon96@users.noreply.github.com>
2022-03-30 22:40:47 +0900

    RestController 사용법 + ResponseStatus기능 ?

--- crud/src/main/java/dev/yeon/crud/post/PostController.java
+++ crud/src/main/java/dev/yeon/crud/post/PostController.java
@@ -3,17 +3,14 @@ package dev.yeon.crud.post;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @ResponseBody
-@RequestMapping("post")
+//@RequestMapping("post")
 public class PostController {
     private static final Logger logger = LoggerFactory.getLogger(PostController.class);
     private final List<PostDto> postList;
@@ -28,4 +25,34 @@ public class PostController {
         this.postList.add(postDto);
     }
 
+    @GetMapping("read-all")
+    public List<PostDto> readPostAll(){
+        logger.info("in read all");
+        return this.postList;
+    }
+
+    @GetMapping("read-one")
+    public PostDto readPostOne(@RequestParam("id") int id){
+        logger.info("in read one");
+        return this.postList.get(id);
+    }
+
+    @PostMapping("update")
+    public void updatePost(@RequestParam("id") int id,
+                           @RequestBody PostDto postDto
+                           ){
+        PostDto targetPost = this.postList.get(id);
+        if(postDto.getTitle() != null) {
+            targetPost.setTitle(postDto.getTitle());
+        }
+        if(postDto.getContent() != null){
+            targetPost.setContent(postDto.getContent());
+        }
+        this.postList.set(id, targetPost);
+    }
+
+    @DeleteMapping("delete")
+    public void deletePost(@RequestParam("id") int id){
+        this.postList.remove(id);
+    }
 }
